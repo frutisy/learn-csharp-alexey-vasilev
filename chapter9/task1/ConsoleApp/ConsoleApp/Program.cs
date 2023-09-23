509965fdcd2bae3b7bfc127c27b44680c47427c5
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-23 17:40:18 +0300

    Add task 1

---
+++ chapter9/task1/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс с символьным свойством";
+
+
+            var obj = new SymbolRangeLimited();
+            obj.Symbol = 'Z';
+            Console.WriteLine($"Символ: {obj.Symbol}");
+
+            obj.Symbol = '1';
+
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ chapter9/task1/ConsoleApp/ConsoleApp/SymbolRangeLimited.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class SymbolRangeLimited
+    {
+        private char _symbol;
+
+        public char Symbol
+        {
+            get
+            {
+                return _symbol;
+            }
+
+            set
+            {
+                if (value >= 'A' && value <= 'Z')
+                {
+                    _symbol = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: значение свойства должно быть символом от 'А' до 'z' включительно.");
+                }
+            }
+        }
+    }
+}
