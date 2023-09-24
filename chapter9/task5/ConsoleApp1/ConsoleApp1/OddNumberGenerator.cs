73e62a854aa262d643d7b96ecd5d1a5234b35ad6
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-24 15:25:09 +0300

    Add task 5

---
+++ chapter9/task5/ConsoleApp1/ConsoleApp1/OddNumberGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class OddNumberGenerator
+    {
+        private static int _currentNumber = 0;
+
+        public static int CurrentValue
+        {
+            get
+            {
+                _currentNumber += 2;
+                return _currentNumber - 1;        
+            }
+
+            set
+            {
+                if (value >= 1)
+                {
+                    _currentNumber = value * 2;
+                }
+                else
+                {
+                   Console.WriteLine("Значение должно быть больше нуля.");
+                 }
+            }
+        }
+    }
+}
---
+++ chapter9/task5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс с генератора нечётных чисел";
+
+
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+
+            OddNumberGenerator.CurrentValue = 5;
+
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+
+            OddNumberGenerator.CurrentValue = 1;
+
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+            Console.WriteLine($"Текущее нечетное число: {OddNumberGenerator.CurrentValue}");
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
