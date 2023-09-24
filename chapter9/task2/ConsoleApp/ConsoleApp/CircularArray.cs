7a1c8382d446718a050b653dbf05728001392427
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-24 15:23:55 +0300

    Add task 2

---
+++ chapter9/task2/ConsoleApp/ConsoleApp/CircularArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class CircularArray
+    {
+        private int[] _numbers;
+        private int _currentIndex;
+
+        public CircularArray(int size)
+        {
+            _numbers = new int[size];
+            _currentIndex = 0;
+        }
+
+        public int PropertyValue
+        {
+            get
+            {
+                if (_numbers.Length == 0)
+                {
+                    throw new InvalidOperationException("Массив пуст.");
+                }
+
+                int value = _numbers[_currentIndex];
+                _currentIndex = (_currentIndex + 1) % _numbers.Length;
+                return value;
+            }
+
+            set
+            {
+                if (_numbers.Length == 0)
+                {
+                    throw new InvalidOperationException("Массив пуст.");
+                }
+
+                _numbers[_currentIndex] = value;
+            }
+        }
+    }
+}
---
+++ chapter9/task2/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс с массивом и циклическим свойством";
+
+
+            var obj = new CircularArray(5);
+
+            Console.WriteLine("Первые два значения:");
+
+            obj.PropertyValue = 1;
+            Console.WriteLine($"Значение свойства: {obj.PropertyValue}");
+
+            obj.PropertyValue = 2;
+            Console.WriteLine($"Значение свойства: {obj.PropertyValue}");
+
+            Console.WriteLine("\nПоследние три значения:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Значение свойства: {obj.PropertyValue}");
+            }
+
+            Console.WriteLine("\nВсе значения:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Значение свойства: {obj.PropertyValue}");
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
