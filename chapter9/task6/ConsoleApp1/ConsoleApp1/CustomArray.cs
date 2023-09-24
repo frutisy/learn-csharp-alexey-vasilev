4a5d9461d721a083d5cf4c3e79e22c51d22c72c9
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-24 16:32:22 +0300

    Add task 6

---
+++ chapter9/task6/ConsoleApp1/ConsoleApp1/CustomArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class CustomArray
+    {
+        private int[] _array;
+        private int _startIndex; 
+
+        public CustomArray(int size, int startIndex)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException("Размер массива должен быть положительным числом.");
+            }
+
+            _array = new int[size];
+            _startIndex = startIndex;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (_IsValidIndex(index))
+                {
+                    return _array[index - _startIndex];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Индекс находится за пределами допустимого диапазона.");
+                }
+            }
+
+            set
+            {
+                if (_IsValidIndex(index))
+                {
+                    _array[index - _startIndex] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Индекс находится за пределами допустимого диапазона.");
+                }
+            }
+        }
+
+        private bool _IsValidIndex(int index)
+        {
+            return index >= _startIndex && index < _startIndex + _array.Length;
+        }
+    }
+}
---
+++ chapter9/task6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс для работы с нестандартными индексами массива";
+
+
+            var customArray = new CustomArray(5, 1);
+
+            customArray[1] = 10;
+            customArray[2] = 20;
+            customArray[3] = 30;
+
+            Console.WriteLine($"Элемент с индексом 1: {customArray[1]}");
+            Console.WriteLine($"Элемент с индексом 2: {customArray[2]}");
+            Console.WriteLine($"Элемент с индексом 3: {customArray[3]}");
+
+
+            Console.WriteLine();
+        }
+    }
+}
