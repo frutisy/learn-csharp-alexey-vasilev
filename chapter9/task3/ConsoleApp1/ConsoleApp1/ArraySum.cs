90457672bf46c528c75269aee80968ba796b680c
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-24 15:24:37 +0300

    Add task 3

---
+++ chapter9/task3/ConsoleApp1/ConsoleApp1/ArraySum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class ArraySum
+    {
+        private int[] _array;
+
+        public int Sum { get { return _array.Sum(); } }
+
+        public ArraySum(int[] array)
+        {
+            _array = array;
+        }
+    }
+}
---
+++ chapter9/task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс с целочисленным массивом и свойством для считывания суммы элементов";
+
+
+            int[] array = { 1, 2, 3, 4, 5 };  
+
+            var obj = new ArraySum(array);
+
+            Console.WriteLine($"Сумма элементов массива равна {obj.Sum}");
+
+
+            Console.WriteLine();
+        }
+    }
+}
