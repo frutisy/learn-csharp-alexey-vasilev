2e0eab1bece220a77878044adf07a9946bddc465
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-24 15:24:53 +0300

    Add task 4

---
+++ chapter9/task4/ConsoleApp1/ConsoleApp1/OctalNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class OctalNumber
+    {
+        private int _integerValue;
+        private string _octalValue;
+
+        public int IntegerValue
+        {
+            set
+            {
+                if (value >= 0) 
+                {
+                    _integerValue = value;
+                    _octalValue = Convert.ToString(value, 8);
+                }
+                else
+                {
+                    Console.WriteLine("Значение должно быть неотрицательным.");
+                }
+            }
+        }
+
+        public string OctalValue { get { return _octalValue; } }
+    }
+}
---
+++ chapter9/task4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс для работы с восьмеричными числами";
+
+
+            var obj = new OctalNumber();
+
+            obj.IntegerValue = -1;
+            obj.IntegerValue = 52;
+
+            Console.WriteLine($"Для целочисленного значения {52} восьмеричный код будет равен {obj.OctalValue}");
+
+
+            Console.WriteLine();
+        }
+    }
+}
