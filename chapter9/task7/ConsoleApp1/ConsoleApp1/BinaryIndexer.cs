d3d8a1edbb9b2c3696821a2651c1d7dd961cc759
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-25 15:31:54 +0300

    Add task 7

---
+++ chapter9/task7/ConsoleApp1/ConsoleApp1/BinaryIndexer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class BinaryIndexer
+    {
+        private string _binaryNumber;
+        private int _counter = 0;
+
+        public BinaryIndexer(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Число должно быть неотрицательным.");
+            }
+
+            _binaryNumber = Convert.ToString(number, 2);
+        }
+
+        public int BinaryNumberLength { get { return _binaryNumber.Length; } }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (_counter == _binaryNumber.Length)
+                {
+                     _counter = 0; 
+                }
+
+                return _binaryNumber[_counter++].ToString();   
+            }
+        }
+    }
+}
---
+++ chapter9/task7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс для представления десятичного числа в двоичное";
+
+
+            int number = 52;
+            var obj = new BinaryIndexer(number);
+
+            Console.WriteLine($"Число {number} в двоичном представлении:");
+            for (int i = 0; i < obj.BinaryNumberLength; i++)
+            {
+                Console.WriteLine($"Бит [{0}]: {obj[0]}");
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
