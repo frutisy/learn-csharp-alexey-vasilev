a15749d48ee939b55ef5b64d1b7f6032321fb1e7
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-09-25 15:32:15 +0300

    Add task 8

---
+++ chapter9/task8/ConsoleApp1/ConsoleApp1/NumberWithIndexedDigits.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class NumberWithIndexedDigits
+    {
+        private int _number;
+
+        public NumberWithIndexedDigits(int number)         
+        {
+            if (_number < 0)
+            {
+                throw new ArgumentException("Число должно быть неотрицательным.");
+            }
+
+            _number = number;
+        }
+        public int Number { get { return _number; } }
+
+        public int this[int index]
+        {
+            set
+            {
+                if (index < 0)
+                    throw new IndexOutOfRangeException("Индекс должен быть неотрицательным");
+
+                int digit = value % 10;
+                _number = _SetDigit(index, digit);
+            }
+        }
+
+        private int _SetDigit(int index, int digit)
+        {
+            int temp = _number;
+            int[] digits = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                digits[i] = temp % 10;
+                temp /= 10;
+            }
+
+            digits[index] = digit;
+            int result = 0;
+            for (int i = 9; i >= 0; i--)
+            {
+                result = result * 10 + digits[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ chapter9/task8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс для замены цифр в разрядах числа";
+
+
+            int number = 12345;
+            var obj = new NumberWithIndexedDigits(number);
+
+            Console.WriteLine($"Число до изменений: {obj.Number}");
+
+            obj[1] = 7;
+            obj[2] = 8;
+
+            Console.WriteLine($"Число после изменений: {obj.Number}");
+
+
+            Console.WriteLine();
+        }
+    }
+}
