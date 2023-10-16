b7216687863bd9537945a3c248d4b87d22efa7ea
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-10-16 17:47:34 +0300

    Add task 10

---
+++ chapter9/task10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Класс с текстовым массивом и индексаторами";
+
+
+            string[] arrayOfStrings = { "Привет", "Мир", "C#", "Программирование" };
+            var textArray = new TextArray(arrayOfStrings);
+
+            // Используем одномерный индексатор для чтения и записи элементов.
+            Console.WriteLine("Исходный текстовый массив:");
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+                Console.WriteLine($"textArray[{i}] = {textArray[i]}");
+            
+            // Запись в элемент массива с помощью одномерного индексатора.
+            textArray[1] = "Вселенная";             
+            Console.WriteLine("\nОбновленный текстовый массив:");
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+                Console.WriteLine($"textArray[{i}] = {textArray[i]}");
+
+            // Используем двумерный индексатор для чтения символов в элементах.
+            Console.WriteLine("\nСимволы в текстовом массиве:");
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                for (int j = 0; j < textArray[i].Length; j++)
+                    Console.WriteLine($"textArray[{i}, {j}] = {textArray[i, j]}");
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ chapter9/task10/ConsoleApp1/ConsoleApp1/TextArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Common;
+
+namespace ConsoleApp1
+{
+    class TextArray
+    {
+        private string[] _textArray;
+
+        public TextArray(string[] array)
+        {
+            _textArray = array;
+        }
+
+        public string this[int index] 
+        { 
+            get
+            {
+                return _textArray[index % _textArray.Length];
+            }
+
+            set
+            {
+                _textArray[index % _textArray.Length] = value;
+            }
+        }
+
+        public char this[int row, int column]
+        {
+            get
+            {
+                string text = _textArray[row % _textArray.Length];
+                column = column % text.Length;
+                return text[column];
+            }
+        }
+    }
+}
