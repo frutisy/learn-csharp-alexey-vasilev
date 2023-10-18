b0a11a6b6e2b06a368c326e8e6f67c0a9af525aa
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-10-18 14:40:13 +0300

    Add task 1

---
+++ chapter10/task1/ConsoleApp1/ConsoleApp1/BaseClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class BaseClass
+    {
+        protected string value;
+
+        public BaseClass(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString() 
+        { 
+            return $"Класс: {this.GetType()}\nТекстовое поле: {value}";
+        }
+    }
+
+    class InheritedClass : BaseClass
+    {
+        protected string data;
+
+        public InheritedClass(string data) : base(String.Empty)
+        {
+            this.data = data;
+        }
+
+        public InheritedClass(string value, string data) : base(value)
+        {
+            this.data = data;
+        }
+
+        public override string ToString() 
+        { 
+            if (value == String.Empty)
+            {
+                return $"Класс: {this.GetType()}\nТекстовое поле: {data}";
+            }
+            else
+            {
+                return $"Класс: {this.GetType()}\nПервое текстовое поле: {value}\nВторое текстовое поле: {data}";
+            }
+        }
+    }
+}
---
+++ chapter10/task1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Базовый и наследуемый классы с защищёнными текстовыми полями";
+
+
+            var baseInstance = new BaseClass("Queen");
+            Console.WriteLine(baseInstance);
+
+            Console.WriteLine();
+
+            var inheritedInstance1 = new InheritedClass("Nirvana");
+            Console.WriteLine(inheritedInstance1);
+
+            Console.WriteLine();
+
+            var inheritedInstance2 = new InheritedClass("The", "Cranberries");
+            Console.WriteLine(inheritedInstance2);
+
+
+            Console.WriteLine();
+        }
+    }
+}
