319f237f8153f1df974add73c371f468a06abf77
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-10-26 14:29:10 +0300

    Add task 2

---
+++ chapter10/task2/ConsoleApp1/ConsoleApp1/BaseClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class BaseClass
+    {
+        protected string text;
+
+        public BaseClass(string text)
+        {
+            this.text = text;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return text.Length;
+            }
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= text.Length)
+                {
+                    throw new IndexOutOfRangeException("Индекс находился вне диапазона!");
+                }
+                else
+                {
+                    return text[index];
+                }
+            }
+        }
+
+        public void SetText()
+        {
+            text = string.Empty;
+        }
+
+        public void SetText(string newText)
+        {
+            text = newText;
+        }
+    }
+
+    class InheritedClass : BaseClass
+    {
+        public int number;
+        
+        public InheritedClass(string text, int value) : base(text)
+        {
+            number = value;
+        }
+
+        public new void SetText() 
+        {
+            base.SetText(); 
+            number = 0;
+        }
+
+        public void SetText(string newText)
+        {
+            base.SetText(newText);
+        }
+
+        public void SetText(int value)
+        {
+            number = value; 
+        }
+
+        public void SetText(string newText, int value)
+        {
+            SetText(newText);
+            number = value;
+        }
+    }
+}
---
+++ chapter10/task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Базовый и производный классы с индексатором и методами";
+
+
+            var inheritedInstance = new InheritedClass("Ode To My Family", 52);
+
+            Console.WriteLine($"Длина текста: {inheritedInstance.Length}");
+            Console.WriteLine($"Символ по индексу 7: {inheritedInstance[7]}");
+
+            inheritedInstance.SetText("Новый текст");
+            Console.WriteLine($"Длина нового текста: {inheritedInstance.Length}");
+
+            inheritedInstance.SetText(99);
+            Console.WriteLine($"Дополнительное поле: {inheritedInstance.number}");
+
+            inheritedInstance.SetText("Еще один текст", 123);
+            Console.WriteLine($"Длина нового текста: {inheritedInstance.Length}, Дополнительное поле: {inheritedInstance.number}");
+
+
+            Console.WriteLine();
+        }
+    }
+}
